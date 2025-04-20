99f2f7135f78d2989b6aa5affa4a2886622898b5
vik <sammydowds1993@gmail.com>
2025-04-20 10:17:45 -0500

    chore: re architect

---
+++ model/plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def plot_values(
+    epochs_seen, examples_seen, train_values, val_values, label="loss"
+):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
+    ax1.plot(epochs_seen, val_values, label=f"Validation {label}", linestyle="-.")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    ax2 = ax1.twiny()
+    ax2.plot(examples_seen, train_values, alpha=0)
+    ax2.set_xlabel("Examples seen")
+
+    fig.tight_layout()
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
--- model/train.py
+++ model/train.py
@@ -1,73 +1,18 @@
+from loss import calc_accuracy_loader, calc_loss_batch, evaluate_model
+from plot import plot_values
 from trained_model import get_small_gpt_2_model
 import torch
-from configs import GPT_SMALL
+from config import GPT_SMALL
 import tiktoken
 from torch.utils.data import DataLoader 
 import time
-from fine_tuning.classification.dataset import SpamDataset
-from fine_tuning.classification.fetcher import SMSSpamFetcher
+from datasets import PropagandaDataset 
+from datasets import PropagandaData 
 import matplotlib.pyplot as plt
 
 TRAINED_MODEL_CACHE = 'propaganda-classifier.pth'
 
-def calc_loss_batch(input_batch, target_batch, model, device):
-    input_batch = input_batch.to(device)
-    target_batch = target_batch.to(device)
-    logits = model(input_batch)[:, -1, :]
-
-    loss = torch.nn.functional.cross_entropy(logits, target_batch)
-    return loss
-
-def calc_accuracy_loader(data_loader, model, device, num_batches=None):
-    model.eval()
-    correct_predictions, num_examples = 0, 0
-
-    if num_batches is None:
-        num_batches = len(data_loader)
-    else:
-        num_batches = min(num_batches, len(data_loader))
-    for i, (input_batch, target_batch) in enumerate(data_loader):
-        if i < num_batches:
-            input_batch = input_batch.to(device)
-            target_batch = target_batch.to(device)
-
-            with torch.no_grad():
-                logits = model(input_batch)[:, -1, :]
-            predicted_labels = torch.argmax(logits, dim=-1)
-            num_examples += predicted_labels.shape[0]
-            correct_predictions += (
-                (predicted_labels == target_batch).sum().item()
-            )
-        else:
-            break
-    return correct_predictions / num_examples
-
-
-def calc_loss_loader(data_loader, model, device, num_batches=None):
-    total_loss = 0.
-    if len(data_loader) == 0:
-        return float("nan")
-    elif num_batches is None:
-        num_batches = len(data_loader)
-    else:
-        num_batches = min(num_batches, len(data_loader))
-        for i, (input_batch, target_batch) in enumerate(data_loader):
-            if i < num_batches:
-                loss = calc_loss_batch(input_batch, target_batch, model, device)
-                total_loss += loss.item()
-            else:
-                break
-        return total_loss / num_batches
-
-def evaluate_model(model, train_loader, val_loader, device, eval_iter):
-    model.eval()
-    with torch.no_grad():
-        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
-        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
-    model.train()
-    return train_loss, val_loss
-
-def train_classifier_simple(
+def train(
         model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter
     ):
     train_losses, val_losses, train_accs, val_accs = [], [], [], []
@@ -109,7 +54,7 @@ def train_classifier_simple(
         val_accs.append(val_accuracy)
     return train_losses, val_losses, train_accs, val_accs, examples_seen
 
-def get_trained_spam_classifier(skip_cache = False):
+def train_classifier(skip_cache = False):
     # init model 
     gpt = get_small_gpt_2_model()
     torch.manual_seed(123)
@@ -139,7 +84,7 @@ def get_trained_spam_classifier(skip_cache = False):
             return gpt
     
     # no cache, train 
-    fetcher = SMSSpamFetcher()
+    fetcher = PropagandaData()
     fetcher.fetch_and_process()
    
     tokenizer = tiktoken.get_encoding('gpt2')
@@ -149,12 +94,12 @@ def get_trained_spam_classifier(skip_cache = False):
     num_epochs = 5
     device = torch.device("cpu")
 
-    train_dataset = SpamDataset(
+    train_dataset = PropagandaDataset(
         csv_file='train.csv',
         max_length=None,
         tokenizer=tokenizer
     )
-    val_dataset = SpamDataset(
+    val_dataset = PropagandaDataset(
         csv_file='validation.csv',
         max_length=None,
         tokenizer=tokenizer
@@ -174,7 +119,7 @@ def get_trained_spam_classifier(skip_cache = False):
         drop_last=False
     )
     
-    train_losses, val_losses, train_accs, val_accs, examples_seen = train_classifier_simple(
+    train_losses, val_losses, train_accs, val_accs, examples_seen = train(
         gpt, train_loader, val_loader, optimizer, device,
         num_epochs=num_epochs, eval_freq=50,
         eval_iter=5
@@ -196,46 +141,5 @@ def get_trained_spam_classifier(skip_cache = False):
      
     return gpt
 
-def classify_text(
-    text, model, tokenizer, device, max_length=None, pad_token_id=50256
-):
-    model.eval()
-    input_ids = tokenizer.encode(text)
-    supported_context_length = model.pos_emb.weight.shape[0]
-
-    if max_length:
-        input_ids = input_ids[:min(
-            max_length, supported_context_length
-        )]
-        input_ids += [pad_token_id] * (max_length - len(input_ids))
-
-    input_tensor = torch.tensor(
-        input_ids, device=device
-    ).unsqueeze(0)
-
-    with torch.no_grad():
-        logits = model(input_tensor)[:, -1, :]
-    
-    predicted_label = torch.argmax(logits, dim=-1).item()
-
-    return "spam" if predicted_label == 1 else "not spam"
-
-
-def plot_values(
-    epochs_seen, examples_seen, train_values, val_values, label="loss"
-):
-    fig, ax1 = plt.subplots(figsize=(5, 3))
-
-    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
-    ax1.plot(epochs_seen, val_values, label=f"Validation {label}", linestyle="-.")
-    ax1.set_xlabel("Epochs")
-    ax1.set_ylabel(label.capitalize())
-    ax1.legend()
-
-    ax2 = ax1.twiny()
-    ax2.plot(examples_seen, train_values, alpha=0)
-    ax2.set_xlabel("Examples seen")
-
-    fig.tight_layout()
-    plt.savefig(f"{label}-plot.pdf")
-    plt.show()
+if __name__ == "__main__":
+    train_classifier()
